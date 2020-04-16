f8e90ad13abc28ad93aa4aa827a7dcdfd0131363
Greivin <greiven@Greivins-MacBook-Pro.local>
2020-04-16 13:18:16 -0700

    secound

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Employee-tracker
+![node](./assets/screenshot.PNG)
+
+Description
+Node.js 
+Employee management system to add and manage Employees and Departments.
---
+++ server.js
@@ -0,0 +1,196 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employeesD_B"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    startEmployeePrompt();
+});
+
+
+// prompt user for what they would like to do
+function startEmployeePrompt(){
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "Which action would you like to take?",
+        choices: ["Add a department", "Add a role", "Add an employee", "View departments", "View roles", "View employees", "Update employee role", "Exit"]
+    }).then(function(answer){
+        console.log(answer.action);
+        switch(answer.action){
+            case "Add a department":
+                //add a department function
+                addDepartment();
+                break;
+            case "Add a role":
+                //add a role function
+                addRole();
+                break;
+            case "Add an employee":
+                //add an employee function
+                addEmployee();
+                break;
+            case "View departments":
+                //view departments function
+                viewDepartments();
+                break;
+            case "View roles":
+                // view roles function
+                viewRoles();
+                break;
+            case "View employees":
+                // view employees function
+                viewEmployees();
+                break;
+            case "Update employee role":
+                // update employee role function
+                updateEmployeeRole();
+                break;
+            case "Exit":
+                connection.end();
+                break;
+        }
+    })
+};
+
+// add new department
+function addDepartment(){
+    inquirer.prompt({
+        name: "newDepartment",
+        type: "input",
+        message: "What is the name of the department you would like to add?"
+    }).then(function(answer){
+        connection.query("INSERT INTO department (name) VALUES (?)", [answer.newDepartment], function(err){
+            if(err) throw err;
+            console.log(`New department - ${answer.newDepartment} created successfully!`);
+            startEmployeePrompt();
+        })
+    });
+}
+
+// add new role
+function addRole(){
+    inquirer.prompt([{
+        name: "newRole",
+        type: "input",
+        message: "Name of the new role:"
+    }, {
+        name: "salary",
+        type: "input", 
+        message: "What is their salary?",
+        validate: validateSalary
+    }, {
+        name: "departmentID",
+        type: "input",
+        message: "What is their department id?",
+        validate: validateID
+    }]).then(function(answer){
+        connection.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [answer.newRole, answer.salary, answer.departmentID], function(err){
+            if(err) throw err;
+            console.log(`New role - ${answer.newRole} created successfully!`);
+            startEmployeePrompt();
+        })
+    });
+}
+
+// add new employee
+function addEmployee(){
+    inquirer.prompt([{
+        name: "firstName",
+        type: "input",
+        message: "What is the employee's first name?"
+    }, {
+        name: "lastName",
+        type: "input", 
+        message: "What is their last name?"
+    }, {
+        name: "roleID",
+        type: "input",
+        message: "What is their role id?",
+        validate: validateID
+    }, {
+        name: "managerID",
+        type: "input",
+        message: "What is their manager's id?",
+        validate: validateID
+    }]).then(function(answer){
+        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answer.firstName, answer.lastName, answer.roleID, answer.managerID], function(err){
+            if(err) throw err;
+            console.log(`New employee - ${answer.firstName} ${answer.lastName} created successfully!`);
+            startEmployeePrompt();
+        })
+    });
+}
+
+// view all departments
+function viewDepartments(){
+    connection.query("SELECT * FROM department", function (err, res){
+        if(err) throw err;
+        console.table(res);
+        startEmployeePrompt();
+    });
+}
+
+// view all roles
+function viewRoles(){
+    connection.query("SELECT * FROM role", function (err, res){
+        if(err) throw err;
+        console.table(res);
+        startEmployeePrompt();
+    });
+}
+
+// view all employees
+function viewEmployees(){
+    connection.query("SELECT * FROM employee", function (err, res){
+        if(err) throw err;
+        console.table(res);
+        startEmployeePrompt();
+    });
+}
+
+// update employee role
+function updateEmployeeRole(){
+    inquirer.prompt([{
+        name: "lastName",
+        type: "input",
+        message: "What is the last name of the employee whose role you would like to change?"
+    }, {
+        name: "newRole",
+        type: "input", 
+        message: "What is their new role id?",
+        validate: validateID
+    }]).then(function(answer){
+        connection.query("UPDATE employee SET ? WHERE ?" , [{role_id: answer.newRole}, {last_name: answer.lastName}], function(err){
+            if(err) throw err;
+            console.log(`Updated role - ${answer.newRole} successfully!`);
+            startEmployeePrompt();
+        })
+    });
+}
+
+// ensures salary is a number
+function validateSalary(salary){
+    if(isNaN(salary)){
+        return "Please enter a valid number for salary";
+    }
+    return true;
+};
+
+// ensures id is a number
+function validateID(id){
+    if(isNaN(id)){
+        return "Please enter a valid number for id";
+    }
+    return true;
+};
+
